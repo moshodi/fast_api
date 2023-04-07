6cd9bf12f2a883a9ad5af40a3a19bc2695fb616f
Brandon Luffman <brandonluff10@gmail.com>
2023-04-07 16:30:54 -0400

    first commit

---
+++ scraper.py
@@ -0,0 +1,29 @@
+from requests_html import HTMLSession
+
+class Scraper():
+    def scrapedata(self, tag):
+        # tag = input('What Tag Bruh \n')
+        # url = f'https://quotes.toscrape.com/tag/{tag}'
+        
+        url = f'https://google.com/search?q={tag}'
+
+        s = HTMLSession()
+        r = s.get(url)
+        print(r.status_code)
+        qlist = []
+        quotes = r.html.find('div.yuRUbf')
+
+        for q in quotes:
+            item = {
+                'Title': q.find('h3', first=True).text.strip(),
+                'Link': q.find('a', first=True).text.strip(),
+            }
+            print(item)
+            qlist.append(item)
+
+        return qlist
+
+quotes = Scraper()
+
+# quotes.scrapedata('life')
+
